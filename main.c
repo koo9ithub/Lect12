8c3937efa7a6d8dcea954c1813d81010788fe862
koo9ithub <kk223704@naver.com>
2018-11-22 15:20:11 +0900

    2nd submission

--- main.c
+++ main.c
@@ -8,13 +8,15 @@ int main(int argc, char *argv[]) {
 	char src[100] = "Programming course";
 	char dst[100];
 	int i;
+	int count=0;
 	
 	for (i=0; src[i] != NULL ; i++) {
 		dst[i] = src[i];
+		count++;
 	}
 	dst[i] = NULL;
 	
-	printf("%s\n", dst);
+	printf("%s(%d)\n", dst, count);
 	
 	return 0;
 }
